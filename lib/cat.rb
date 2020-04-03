016690897f05ef1adf59060ae2ee53d50d071543
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-03 17:05:39 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,2 +1,20 @@
 class Cat 
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  def initialize(name, owner = nil)
+    @name = name 
+    @mood = "nervous"
+  end 
+  
+  def save 
+    
+    
 end 
