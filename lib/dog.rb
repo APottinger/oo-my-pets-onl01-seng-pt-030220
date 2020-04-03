90e4fc020f123721c3fd4fcfa219e9a2ed9f1e23
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-03 17:09:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,22 @@
 class Dog 
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  def initialize(name, owner = nil)
+    @name = name
+    @mood = "nervous"
+    save 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+    
 end 
