5123a6f989a50def17d9d29dd522e5f028a9bb9a
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-03 17:01:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,27 @@
 class Owner
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
+  
   attr_reader :name, :species
   
   def initialize(species = human)
     @name, @species = name, species
-    
+    save
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def say_species(species)
+    "Hi, I am a #{species}!"
+  end 
   
 end 
+
+Owner.all.count
